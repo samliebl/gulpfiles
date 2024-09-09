1bac91a654544b1a099f8df4dc817ad1e4a5b026
S T <stliebl@gmail.com>
2024-09-09 14:09:31 -0400

    Wanting to build personal website this way, so adding some Nunjucks templating.

---
+++ data/index.js
@@ -0,0 +1,20 @@
+export const data = {
+    site: {
+        title: 'SL',
+        description: 'Sam Liebl’s personal website, and professional too I guess',
+        author: 'Sam Liebl',
+        links: [{
+                rel: 'preconnect',
+                href: 'https://rsms.me/'
+            },
+            {
+                rel: 'stylehseet',
+                href: 'https://rsms.me/inter/inter.css'
+            },
+            {
+                rel: 'stylehseet',
+                href: 'css/index.css'
+            }
+        ]
+    }
+};
