5e9869a8e53e30542c8f5ecbe64e74c43be4c5f5
S T <stliebl@gmail.com>
2024-09-27 07:05:34 -0400

    Fixed issue, going to merge

--- data/index.js
+++ data/index.js
@@ -15,6 +15,21 @@ export const data = {
                 rel: 'stylehseet',
                 href: 'css/main.css'
             }
+        ],
+        js: ['js/main.js'],
+        navitems: [
+            { anchor: 'Home', href: 'index.html' },
+            { anchor: 'Poetry', href: 'poems/poems.html' },
+            { anchor: 'Skills', href: 'skills.html' },
+            { anchor: 'GitHub', href: 'https://github/samliebl/' },
+            { anchor: 'I make music', href: 'https://soundcloud/boutiquebeats/' },
+            { anchor: 'Instagram', href: 'https://instagram/saminpractice/' }
         ]
+    },
+    social: {
+        linkedin: 'https://linkedin/in/samliebl.com/',
+        soundcloud: 'https://soundcloud/boutiquebeats/',
+        github: 'https://github/samliebl/',
+        instagram: 'https://instagram/saminpractice/'
     }
 };
